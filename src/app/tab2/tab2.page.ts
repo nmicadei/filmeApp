a1e5bfa277e4dc54b7778625b3ee29b83f3c5ec2
Etec 244 <e244ti@outlook.com>
2021-09-17 15:03:43 -0300

    filmeapp
    aplicaçao de filme

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'nao vai compartilhar',
+      subHeader: 'ja falei que nao vai',
+      message: 'irmao vai toma caju.',
+      buttons: ['OK']
+    });
 
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+  
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'nao tem suco tem suco vai comprar.',
+      duration: 5000
+    });
+    toast.present();
+  }
 }
